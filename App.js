25b2c65c8b37f9aa02e946cf241bc6ce561fc160
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-03-09 16:08:25 +0100

    style refactoring

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import {Image, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{ width: 305, height: 159 }} />
-      <Text style={{color: '#888', fontSize: 18}}>To share a photo from your phone with a friend, just press the button below!</Text>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
     </View>
   );
 }
@@ -17,4 +17,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  }, 
 });
