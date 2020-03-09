fa708d5c6ea3a45df4c7487207f8463f5dafa19e
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-03-09 16:13:41 +0100

    TouchableOpacity component added

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
       <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -26,5 +31,14 @@ const styles = StyleSheet.create({
     color: '#888',
     fontSize: 18,
     marginHorizontal: 15,
+  }, 
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   }, 
 });
